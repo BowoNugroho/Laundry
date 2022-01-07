7167a58d34b58f1a8907203d9ae77c907d436d5b
BowoNugroho <bowotkj10@gmail.com>
2022-01-07 19:03:21 +0700

    memperbaiki models dan migration

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $guarded =['id'];
+
+    public function gender()
+    {
+    return $this->belongsTo(Gender::class);
+    }
+    public function status()
+    {
+    return $this->belongsTo(Status::class);
+    }
+}
